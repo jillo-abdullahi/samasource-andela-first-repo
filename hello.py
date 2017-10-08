602c98908cb3ef685b9deec9e0d347d5ab60b1b0
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-10-08 19:34:55 +0300

    More lines now?

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("Everything is awesome!")
 print("I just solved a proxy issue on my Ubuntu terminal. Yay!")
 print("Now is the time to start learning some python")
+print("This is going to be amazing")
