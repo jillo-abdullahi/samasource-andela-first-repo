1a0ccaca1deada98abb1ec65b4234dc9f714a8a2
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-10-09 19:49:16 +0300

    Added function to divide two numbers

--- hello.py
+++ hello.py
@@ -1,4 +1,14 @@
+#Some random printouts
 print("Everything is awesome!")
 print("I just solved a proxy issue on my Ubuntu terminal. Yay!")
 print("Now is the time to start learning some python")
 print("This is going to be amazing")
+
+
+#Function to add two numbers
+
+def divide_two_numbers(a,b):
+	return a / b
+
+
+print "The answer is %d. " % (divide_two_numbers(150,3))
