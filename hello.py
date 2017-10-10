06f997db71d5e2225b9df170e59eed835c11da9a
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-10-10 19:17:17 +0300

    Played around with some random lines

--- hello.py
+++ hello.py
@@ -11,4 +11,10 @@ def divide_two_numbers(a,b):
 	return a / b
 
 
-print "The answer is %d. " % (divide_two_numbers(150,3))
+#print "The answer is %d. " % (divide_two_numbers(150,3))
+print divide_two_numbers(150,3)
+
+def add_two_numbers(a,b):
+	print a + b
+
+add_two_numbers(2,8)
