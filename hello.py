c6748c51737f9b38fe7cb6be6a008390748b9957
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-10-10 22:24:13 +0300

    Removed functions

--- hello.py
+++ hello.py
@@ -3,18 +3,3 @@ print("Everything is awesome!")
 print("I just solved a proxy issue on my Ubuntu terminal. Yay!")
 print("Now is the time to start learning some python")
 print("This is going to be amazing")
-
-
-#Function to add two numbers
-
-def divide_two_numbers(a,b):
-	return a / b
-
-
-#print "The answer is %d. " % (divide_two_numbers(150,3))
-print divide_two_numbers(150,3)
-
-def add_two_numbers(a,b):
-	print a + b
-
-add_two_numbers(2,8)
