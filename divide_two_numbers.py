8c288a4d791a048581a8c64b8aa76accd96f2d76
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-10-12 00:42:18 +0300

    [Update] Test and function updated

--- divide_two_numbers.py
+++ divide_two_numbers.py
@@ -1,5 +1,9 @@
 def divide_two_numbers(x,y):
+	#Raise exception to avoid a divide-by-zero
 	if y == 0:
 		raise ValueError('Cannot divide by zero')
+
 	return x / y
+
+
 
--- test_divide_two_numbers.py
+++ test_divide_two_numbers.py
@@ -3,12 +3,14 @@ import divide_two_numbers
 
 class TestDivision(unittest.TestCase):
 
-	def test_divide_two_numbers(self):
+	def test_basic_division(self):
 		self.assertEqual(divide_two_numbers.divide_two_numbers(10,2), 5)
+	def test_edge_case(self):
 		self.assertEqual(divide_two_numbers.divide_two_numbers(-15,-3), 5)
-		self.assertEqual(divide_two_numbers.divide_two_numbers(100,2), 50)
-		self.assertEqual(divide_two_numbers.divide_two_numbers(5,2), 2.5)
+	def test_edge_case(self):
 		self.assertEqual(divide_two_numbers.divide_two_numbers(-500,2), -250)
+	def test_float_answer(self):
+		self.assertEqual(divide_two_numbers.divide_two_numbers(5,2), 2.5)
 
 if __name__ == '__main__':
 	unittest.main()
