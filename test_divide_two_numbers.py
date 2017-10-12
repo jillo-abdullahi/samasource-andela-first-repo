ea99acdb543fcacd7b7456171257b357540eff5d
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-10-12 15:29:22 +0300

    [Update] Last test case updated

--- test_divide_two_numbers.py
+++ test_divide_two_numbers.py
@@ -1,16 +1,16 @@
 import unittest
-import divide_two_numbers
+from divide_two_numbers import divide_two_numbers
 
 class TestDivision(unittest.TestCase):
 
 	def test_basic_division(self):
-		self.assertEqual(divide_two_numbers.divide_two_numbers(10,2), 5)
+		self.assertEqual(divide_two_numbers(10,2), 5)
 	def test_edge_case(self):
-		self.assertEqual(divide_two_numbers.divide_two_numbers(-15,-3), 5)
-	def test_edge_case(self):
-		self.assertEqual(divide_two_numbers.divide_two_numbers(-500,2), -250)
+		self.assertEqual(divide_two_numbers(-15,-3), 5)
+	def test_edge_case_two(self):
+		self.assertEqual(divide_two_numbers(-500,2), -250)
 	def test_float_answer(self):
-		self.assertEqual(divide_two_numbers.divide_two_numbers(5,2), 2.5)
+		self.assertEqual(divide_two_numbers(5,2), 2.5)
 
 if __name__ == '__main__':
 	unittest.main()
